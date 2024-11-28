bfb4cb867a05e8a6244d331ed84fb20bfdaf4fe8
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-11-28 12:53:28 +0530

    functionandloop

---
+++ day4whileLoop/index.js
@@ -0,0 +1,69 @@
+// for(let i=1;i<=10;i++){
+//   console.log(i)
+// }
+
+// while loop
+
+// while(condition ){ // syntax
+
+// }
+
+let number = 2; // initialization
+
+while (number <= 15) { // condition// false
+  // console.log(number);
+  // if(number%2==0){
+  //   console.log(`even  ${number}`); // print only even numbers
+  // }
+  number++; // increment number
+  if(number%2==0){
+    console.log(`even  ${number}`); // print only even numbers
+  }
+  // console.log(number);
+}
+
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
+//11
+
+
+// do while
+// first execute code 
+// then check the condition
+
+
+// syntax of do while loop
+let numValue = 1
+do{
+  numValue++ 
+console.log(numValue)
+ // increment number each time before checking condition again  // false
+}while(numValue <=10)
+
+
+
+  /// how to create normal function 
+
+  sum(12,5)// arguments
+  function sum(numb1,numb2){ // parameters
+    // console.log("sum")
+    console.log(numb1 +numb2)
+  }
+
+
+  function subtract(a,b,c){ // parameters
+console.log(a-b-c)
+  }
+
+
+  subtract(120,9,50)
+
+
+
