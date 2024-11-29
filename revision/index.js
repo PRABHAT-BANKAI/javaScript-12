6c7ee074034cd6d5789747458726d1fbb6a420f0
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-11-29 12:45:18 +0530

    forlooprevision

---
+++ revision/index.js
@@ -0,0 +1,78 @@
+// for loop
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+// let name = "prabhat"
+// let i = 0 initialization variable
+
+// for (initialization let i = 1; condition  i <= 10; increment  index++) {
+//   const element = array[index];
+
+// }
+
+for (let i = 1; i <= 10; i = i + 2) {
+  console.log(i);
+}
+
+//  1
+//2
+//3
+//  4
+//5
+//6
+//  7
+//8
+//9
+//
+
+//ouput
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+let string = "";
+console.log("125" + 2); //concatenate
+
+for (let i = 1; i <= 6; i++) {
+  string += i;
+}
+console.log(string);
+//1
+// 12
+// 123
+// 1234
+// 12345
+// 123456
+
+//123456
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+  console.log(sum);
+}
+
+console.log(sum);
+
+let count = 0
+
+for(let i=0;i<=5;i++){
+count++
+}
+
+
+console.log(count)
+
+
+
